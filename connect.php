e1bbc1d9246a7936ed165ea22b44bf043177a3b1
mdshaheenmiah <mdshaheen60101@gmail.com>
2024-03-24 16:13:57 +0600

    first commit

---
+++ connect.php
@@ -0,0 +1,9 @@
+
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'student1';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
